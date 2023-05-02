46da6a10a2db0d611d9da40c44bb81fb04b4dec2
Kriti Vajjhula <avajjh@uw.edu>
2023-05-02 13:08:53 -0700

    create new file and make vcd responsive

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -80,6 +80,7 @@ export function ProjectsPage(props) {
                         </Link>
                     </MDBCol>
                 </MDBRow> */}
+                
             </MDBContainer>
         </div>
 
